1ed15b36837420fc307be99261fb64efa880ad14
Ajas333 <ajaskm333@gmail.com>
2023-12-21 12:31:03 +0530

    image edited

--- shoplux/user_log/models.py
+++ shoplux/user_log/models.py
@@ -2,8 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.conf import settings 
-# from django.utils import timezone
-# from django.contrib.auth.models import AbstractBaseUser, UserManager,PermissionsMixin,Permission,Group
 
 
 
